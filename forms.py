d41044b654d235a75d9b21bbda0b3c32284e7235
Alexander De_Laurentiis <delaurentiisa99@gmail.com>
2021-02-05 18:25:32 -0500

    Updated contents

--- forms.py
+++ forms.py
@@ -1,22 +1,30 @@
-from wtforms import Form, BooleanField, StringField, PasswordField, validators
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, TextAreaField
+from wtforms.validators import InputRequired, EqualTo, Regexp
 
+class RegisterForm(FlaskForm):
+	username=StringField('Username',
+						  validators=[
+						  InputRequired(message='You can not leave this field empty.'),
+						  Regexp('^([A-Za-z0-9]{6,18})$', message='You can only use letters and numbers.')])
+	password=PasswordField('Password',
+							validators=[
+							InputRequired(message='You can not leave this field empty.'),
+							Regexp('^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#\$%\^&\*])', message='You at least one uppercase, lowercase, a number, and special character.')])
+	ConfirmPassword=PasswordField('Confirm Password',
+									validators=[
+									InputRequired(message='You can not leave this field empty.'),
+									EqualTo('password', message='Passwords do NOT match.')])
+	email=StringField('Email',
+					  validators=[
+					  InputRequired(message='You can not leave this field empty.'),
+					  Regexp('([A-Z0-9a-z]+)[@]([A-Z0-9a-z]+)[\.]([A-Z0-9a-z])+', message='You can not leave this field empty.')])
 
-class RegisterForm(Form):
-    username = StringField('Username', [validators.Length(min=6, max=18)])
-    email = StringField('Email Address', [validators.Length(min=6, max=35)])
-    password = PasswordField('New Password', [
-        validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords must match')])
-    ConfirmPassword = PasswordField('Repeat Password')
+class LogInForm(FlaskForm):
+	username=StringField('Username', validators=[InputRequired(message='You can not leave this field empty.')])
+	password=PasswordField('Password', validators=[InputRequired(message='You can not leave this field empty.')])
 
-
-class LogInForm(Form):
-    username = StringField('Username',[validators.DataRequired()])
-    password = StringField('Password',[validators.DataRequired()])
-
-
-class PageDataForms(Form):
-    a = StringField()
-
-
-
+class PageDataForms():
+    TabEdit=StringField(validators=[InputRequired()])
+    HeaderEdit=StringField(validators=[InputRequired()])
+    DescriptionEdit=TextAreaField(validators=[InputRequired()])
